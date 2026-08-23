0f0a351639b2f4e20f33b63cf0b674b4f33cc897
AnujAcharya2338 <anujacharya877@gmail.com>
2026-08-23 22:38:53 +0545

    Responds to Hello and Date and Time

--- Python-Task1-VoiceAssistance/assistant.py
+++ Python-Task1-VoiceAssistance/assistant.py
@@ -1,7 +1,9 @@
 import speech_recognition as sr
-
+import datetime
 
 att = sr.Recognizer()
+date = datetime.date.today()
+time = datetime.datetime.now().time()
 
 try:
     with sr.Microphone() as source:
@@ -9,8 +11,17 @@ try:
         att.adjust_for_ambient_noise(source)
         audio = att.listen(source)
         
-    text = att.recognize_google(audio)
+    text = att.recognize_google(audio).lower()
     print(text)
+    
+    if text == "hello":
+        print("Hello Matey! How can i help you?")
+
+    elif text == "date":
+        print(f"Today date is: {date}")
+
+    elif text == "time":
+        print(f"Today time is: {time}")
 
 except sr.UnknownValueError:
     print(f"Couldnt understand init mate")
@@ -18,6 +29,10 @@ except sr.RequestError as e:
     print(f"Didn't get your message aye brother ; {e}")
 except Exception as e:
     print(f"Someother problem as {e}")
+
+
+
+
 
 
 
